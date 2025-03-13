6548a28341666f53bb15c49ce09fdacc627e569a
Tailosrx <kevinrx03@gmail.com>
2025-03-13 19:10:57 +0100

    #

--- NorthwindSolutions/app.js
+++ NorthwindSolutions/app.js
@@ -1,15 +1,8 @@
 import express from 'express';
 import dbconnection from './dbconnection.js';
-<<<<<<< HEAD
 
 
 const PORT = process.env.PORT || 3000;
-=======
-import product from './backend/routes/products.js';
-import customers from './backend/routes/customers.js';
-const PORT = 3000;
-// const { Pool } = pkg;
->>>>>>> e18af7571eca4074387bec1649a0a087d51c9894
 // import pg from 'pg';
 //import cors from 'cors';
 import product from './backend/routes/products.js';
