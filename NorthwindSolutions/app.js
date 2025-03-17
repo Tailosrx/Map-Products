12b13dc2f9aa334ac118817a60be83e58f01d4f8
Angel <ancarfer@ibadia.cat>
2025-03-17 17:47:26 +0100

    added orders and customers

--- NorthwindSolutions/app.js
+++ NorthwindSolutions/app.js
@@ -3,6 +3,7 @@ import dbconnection from './dbconnection.js';
 //TODO: import routes
 import product from './backend/routes/products.js';
 import customers from './backend/routes/customers.js';
+import orders from './backend/routes/orders.js';
 
 const PORT = 3000;
 // const { Pool } = pkg;
@@ -16,6 +17,7 @@ app.use(express.static('public'));
 // app.use(express.json());
 app.use('/products', product);
 app.use('/customers', customers);
+app.use('/orders', orders);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
