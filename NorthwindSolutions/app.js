a0c0567afc77894d949741d8ed682a0b855b19f5
Tailosrx <kevinrx03@gmail.com>
2025-03-24 16:09:37 +0100

    ruta pagina default y fetch (falta arreglar)

--- NorthwindSolutions/app.js
+++ NorthwindSolutions/app.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import path from 'path';
 import dbconnection from './dbconnection.js';
 import product from './backend/routes/products.js';
 import customers from './backend/routes/customers.js';
@@ -6,22 +7,27 @@ import suppliers from './backend/routes/suppliers.js';
 import categories from './backend/routes/categories.js';
 import employees from './backend/routes/employees.js';
 
-
 const PORT = 3000;
-// const { Pool } = pkg;
-// import pg from 'pg';
-// import cors from 'cors';
 
 const app = express();
 
-// app.use(cors({origin: '*'}));
-app.use(express.static('public'));
-// app.use(express.json());
+// import cors from 'cors';
+// app.use(cors({ origin: '*' }));
+app.use(express.static('frontend'));
+
+// API routes
 app.use('/products', product);
 app.use('/customers', customers);
 app.use('/suppliers', suppliers);
 app.use('/categories', categories);
 app.use('/employees', employees);
+
+// Creamos una ruta para la página principal (index3.html)
+app.get('/', (req, res) => {
+  const filePath = path.join(__dirname, 'frontend', 'index3.html');
+  console.log('Serving file:', filePath);
+  res.sendFile(filePath); // Serve the file directly
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
