dfabfd0536c3c212d83183bb4c0eae34ac2edd80
Angel <ancarfer@ibadia.cat>
2025-03-24 16:57:42 +0100

    added put

--- NorthwindSolutions/backend/routes/orders.js
+++ NorthwindSolutions/backend/routes/orders.js
@@ -32,6 +32,17 @@ router.post('/', async (req,res)=>{
     await pgClient.end();
 });
 
+router.put('/:id', async (req,res)=>{
+    let pgClient = new pg.Client(dbconnection);
+    await pgClient.connect();
+    const data = req.body
+    const result = await pgClient.query('update orders set "CustomerID"=$1, "EmployeeID"=$2, "OrderDate"=$3, "RequiredDate"=$4, "ShippedDate"=$5, "ShipVia"=$6, "Freight"=$7, "ShipName"=$8, "ShipAddress"=$9, "ShipCity"=$10, "ShipRegion"=$11, "ShipPostalCode"=$12, "ShipCountry"=$13 where "OrderID"=$14',
+        [data.CustomerID, data.EmployeeID, data.OrderDate, data.RequiredDate, data.ShippedDate, data.ShipVia, data.Freight, data.ShipName, data.ShipAddress, data.ShipCity, data.ShipRegion, data.ShipPostalCode, data.ShipCountry, req.params.id])
+    // res.json(result.rows[0]);
+    res.json({message: "Producte actualitzat correctament", data: data});
+    await pgClient.end();
+});
+
 
 
 
